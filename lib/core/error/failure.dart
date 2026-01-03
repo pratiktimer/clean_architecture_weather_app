f39858d955e84953f60cc3319bc8bb960fada66b
praban <pbanawalkar2@gmail.com>
2026-01-03 12:28:30 +0530

    Flutter TDD Clean Architecture Course [5] – Contracts of Data Sources [Part1]

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -4,4 +4,11 @@ abstract class Failure extends Equatable {
   // If the subclasses have some properties, they'll get passed to this constructor
   // so that Equatable can perform value comparison.
   const Failure([List properties = const <dynamic>[]]) : super();
+
+  
 }
+
+// General failures
+abstract class ServerFailure extends Failure {}
+
+abstract class CacheFailure extends Failure {}
