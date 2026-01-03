8c36ed7f92508950c07a7a125ee45cd8ed7d451a
praban <pbanawalkar2@gmail.com>
2026-01-03 12:39:58 +0530

    Flutter TDD Clean Architecture Course [5] – Contracts of Data Sources [Part2]

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,8 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
 import '../../../../core/error/failure.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../entities/number_trivia.dart';
-import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import '../repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
